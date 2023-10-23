9b3c8cb2cd1748e6b8227844ca6b86eb5724bfa4
Jh-jaehyuk <jh.jaehyuk213@gmail.com>
2023-10-23 19:13:22 +0900

    Update files

--- Examples/Regression/Linear Regression Sample.py
+++ Examples/Regression/Linear Regression Sample.py
@@ -1,14 +1,24 @@
-from sklearn.datasets import fetch_california_housing
 import numpy as np
-import pandas as pd
 import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
 
-dataset = fetch_california_housing()
-x_data = dataset.data
+dataset = open("Regression/Linear Regression.txt", 'r')
+text = dataset.readlines()
+dataset.close()
+
+x_data = []
+y_data = []
+
+for t in text:
+    data = t.split()
+    x_data.append(float(data[0]))
+    y_data.append(float(data[1]))
+
+x_data.sort()
+y_data.sort()
+
 x_data = np.asarray(x_data, dtype=np.float32)
-y_data = dataset.target
 y_data = np.asarray(y_data, dtype=np.float32)
 
 plt.figure(1)
@@ -16,7 +26,7 @@ plt.plot(x_data, y_data, 'ro')
 plt.xlabel('x-axis')
 plt.ylabel('y-axis')
 plt.title('Dataset')
-plt.show()
+#plt.show()
 
 # Hyper-parameters define
 input_size = 1
@@ -31,8 +41,12 @@ model = nn.Linear(input_size, output_size)
 criterion = nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-print(x_data.shape) #(20640, 8)
-print(y_data.shape) #(20640,)
+print(x_data.shape)
+print(y_data.shape)
+
+if len(x_data.shape) == 1 and len(y_data.shape) == 1:
+    x_data = np.expand_dims(x_data, axis=-1)
+    y_data = np.expand_dims(y_data, axis=-1)
 
 # train model
 for epoch in range(1, num_epochs + 1):
@@ -51,3 +65,9 @@ for epoch in range(1, num_epochs + 1):
 
     if epoch % 10 == 0:
         print(f'Epoch [{epoch}/{num_epochs}], Loss: {loss.item():.4f}.')
+
+predicted = model(torch.from_numpy(x_data)).detach().numpy()
+plt.plot(x_data, y_data, 'ro', label='Original data')
+plt.plot(x_data, predicted, label='Fitted Line')
+plt.legend()
+plt.show()
