b5323e9e3a215353ab08f900ab59003185b04e1e
Jh-jaehyuk <jh.jaehyuk213@gmail.com>
2023-11-01 16:38:18 +0900

    Update files

---
+++ Examples/Neural Network/Chess/ChessEngine.py
@@ -0,0 +1,16 @@
+class GameState():
+    def __init__(self):
+        # The first character represents the color of the piece
+        # The second character represents the type of the piece
+        # "--" represents an empty space with no pieces.
+        self.board = [
+            ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
+            ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
+            ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
+            ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']]
+        self.WhiteToMove = True
+        self.moveLog = []
---
+++ Examples/Neural Network/Minimax/Tic-Tac-Toe.py
@@ -0,0 +1,5 @@
+import pygame
+import sys
+import random
+from pygame.locals import *
+from 
