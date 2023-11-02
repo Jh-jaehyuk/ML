6c4b772e1e479ef4f454e2efa428433501823e78
Jh-jaehyuk <jh.jaehyuk213@gmail.com>
2023-11-02 16:40:54 +0900

    Update files

--- Examples/Neural Network/Tutorials-Pytorch/For Quickstart.py
+++ Examples/Neural Network/Tutorials-Pytorch/For Quickstart.py
@@ -11,19 +11,132 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 
 # 데이터 작업하기
+# 학습 데이터 내려받기
 training_data = datasets.FashionMNIST(
-    root='MNIST',
+    root='../Tutorials-Pytorch',
     train=True,
     download=True,
     transform=ToTensor(),
 )
-
+# 테스트 데이터 내려받기
 test_data = datasets.FashionMNIST(
-    root='MNIST',
+    root='../Tutorials-Pytorch',
     train=False,
     download=True,
     transform=ToTensor(),
 )
 
-print(training_data)
-print(test_data)
+batch_size = 64
+# 데이터로더 생성하기
+train_dataloader = DataLoader(training_data, batch_size=batch_size)
+test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+for X, y in test_dataloader:
+    print(f'Shape of X [N, C, H, W]: {X.shape}')
+    print(f'Shape of y: {y.shape} {y.dtype}')
+    break
+
+# 모델 만들기
+device = 'mps'
+print(f'Using {device} device')
+
+
+# 모델 정의하기
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28 * 28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+
+model = NeuralNetwork().to(device)
+print(model)
+
+
+# 모델 매개변수 최적화하기
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # 예측 오류 계산
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # 역전파
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f'loss: {loss:>7f} [{current:>5d}/{size:>5d}]')
+
+
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+
+# epochs = 5
+# for t in range(epochs):
+#     print(f'Epoch {t + 1}\n----------------------')
+#     train(train_dataloader, model, loss_fn, optimizer)
+#     test(test_dataloader, model, loss_fn)
+# print('Done!')
+
+
+# 모델 저장하기
+torch.save(model.state_dict(), "model.pth")
+print("Saved PyTorch Model State to model.pth")
+
+# 모델 불러오기
+model = NeuralNetwork().to(device)
+model.load_state_dict(torch.load("model.pth"))
+
+# 불러온 모델을 사용하여 예측하기
+classes = [
+    "T-shirt/top",
+    'Trouser',
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
+
+model.eval()
+x, y = test_data[0][0], test_data[0][1]
+with torch.no_grad():
+    x = x.to(device)
+    pred = model(x)
+    predicted, actual = classes[pred[0].argmax(0)], classes[y]
+    print(f'Predicted: "{predicted}", Actual: "{actual}"')
